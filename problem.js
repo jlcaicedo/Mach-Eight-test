1746cde4b57019fb3c4ed2a79734695427d5a79b
Jose Caicedo <jlcaicedo@outlook.com>
2021-12-12 19:25:00 -0500

    Summit problem

--- problem.js
+++ problem.js
@@ -2,7 +2,6 @@
 const api_url = "https://mach-eight.uc.r.appspot.com/";
 
 // define integer input
-const intInput = 139
 
 // get API data
 const getData = async (url) => {
@@ -17,24 +16,33 @@ const getData = async (url) => {
     return data;
 }
 
+function find() {
+    getData(api_url)
+        .then(function (data) {
+            const intInput = document.getElementById("filter_height").value;
+            const tablePlayers = document.getElementById("players")
 
-getData(api_url)
-    .then(function (data) {
-        const players = []
-        const results = []
-        const originalPlayers = data.values
-        originalPlayers.map((element, index1) => {
-            let result = intInput - element.h_in
-            players.push([index1, result])
-            players.forEach((factor, index2) => {
-                if(element.h_in == factor[1]) {
-                    results.push([index1, index2])
-                }
+            const players = []
+            const results = []
+            const originalPlayers = data.values
+            originalPlayers.map((element) => {
+                let result = intInput - element.h_in
+                players.push([element, result])
+                players.forEach((factor) => {
+                    if (element.h_in == factor[1]) {
+                        results.push([`${element.first_name} ${element.last_name}`, `${factor[0].first_name} ${factor[0].last_name}`])
+                    }
+                })
             })
-
+            console.log(results)
+            if (results.length > 0) {
+                tablePlayers.innerHTML = results.map(player => {
+                    return (`<tr><td>${player[0]}</td><td>${player[1]}</td></tr>`);
+                })
+            } else {
+                tablePlayers.innerHTML = `<tr><td colspan="2" class="text-center">No matches found</td></tr>`
+            }
         })
-        console.log(results)
-        console.log(originalPlayers)
-    })
+}
 
 
